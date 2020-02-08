c78a79db06e60035f87da6713de971d32b21d9ab
thinknehru <thinknehru@gmail.com>
2020-02-08 19:01:21 +0300

    Create the character list activity

---
+++ app/src/main/java/com/thinknehru/brokenbad/ui/CharacterListActivity.java
@@ -0,0 +1,16 @@
+package com.thinknehru.brokenbad.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.thinknehru.brokenbad.R;
+
+public class CharacterListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_character_list);
+    }
+}
