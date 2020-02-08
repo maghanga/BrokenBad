79de3589fc29652744f2f3e2c7d9217f8c465993
thinknehru <thinknehru@gmail.com>
2020-02-08 18:17:41 +0300

    Create all packages and add main activity to ui package

--- app/src/main/java/com/thinknehru/brokenbad/ui/MainActivity.java
+++ app/src/main/java/com/thinknehru/brokenbad/ui/MainActivity.java
@@ -1,8 +1,10 @@
-package com.thinknehru.brokenbad;
+package com.thinknehru.brokenbad.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.thinknehru.brokenbad.R;
 
 public class MainActivity extends AppCompatActivity {
 
