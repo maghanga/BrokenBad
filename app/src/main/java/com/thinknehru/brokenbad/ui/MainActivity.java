e34f4d1c07abbc3ba85b0246e9e2b29878f814ab
thinknehru <thinknehru@gmail.com>
2020-02-08 18:52:56 +0300

    Import ButterKnife in project build file

--- app/src/main/java/com/thinknehru/brokenbad/ui/MainActivity.java
+++ app/src/main/java/com/thinknehru/brokenbad/ui/MainActivity.java
@@ -3,15 +3,22 @@ package com.thinknehru.brokenbad.ui;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 import com.thinknehru.brokenbad.R;
 
 public class MainActivity extends AppCompatActivity {
+//    @BindView(R.id.viewCharactersButton)
+//    Button mViewCharactersButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        ButterKnife.bind(this);
+
     }
 }
 
