c9bfb4145e0f91edb5aba5c9d100d9b25b9eea32
thinknehru <thinknehru@gmail.com>
2020-02-10 23:10:05 +0300

    Override and define the methods in the Fragment Pager Adapter

--- app/src/main/java/com/thinknehru/BrokenBad/adapters/CharacterPagerAdapter.java
+++ app/src/main/java/com/thinknehru/BrokenBad/adapters/CharacterPagerAdapter.java
@@ -1,4 +1,34 @@
 package com.thinknehru.BrokenBad.adapters;
 
-public class CharacterPagerAdapter {
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+import com.thinknehru.BrokenBad.models.Character;
+import com.thinknehru.BrokenBad.ui.CharacterDetailFragment;
+
+import java.util.List;
+
+public class CharacterPagerAdapter extends FragmentPagerAdapter {
+    private List<Character> mCharacters;
+
+    public CharacterPagerAdapter(FragmentManager fm, int behavior, List<Character> characters){
+        super(fm, behavior);
+        mCharacters = characters;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return CharacterDetailFragment.newInstance(mCharacters.get(position));
+    }
+
+    @Override
+    public int getCount(){
+        return mCharacters.size();
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return mCharacters.get(position).getName();
+    }
 }
