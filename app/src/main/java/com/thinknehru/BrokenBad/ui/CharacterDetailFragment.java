b5fa6c9553104d8510ff7b8208547e770172e5b7
thinknehru <thinknehru@gmail.com>
2020-02-10 23:02:48 +0300

    Deseralize data in the character list fragment and use the information to display in the set views

--- app/src/main/java/com/thinknehru/BrokenBad/ui/CharacterDetailFragment.java
+++ app/src/main/java/com/thinknehru/BrokenBad/ui/CharacterDetailFragment.java
@@ -8,26 +8,81 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
 
+import com.squareup.picasso.Picasso;
 import com.thinknehru.BrokenBad.R;
+import com.thinknehru.BrokenBad.models.Character;
+
+import org.parceler.Parcels;
+
+import java.lang.reflect.Array;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class CharacterDetailFragment extends Fragment {
+    @BindView(R.id.characterImageView)
+    ImageView mImageLabel;
+    @BindView(R.id.characterNameTextView)
+    TextView mNameLabel;
+    @BindView(R.id.portrayedTextView) TextView mPortrayedLabel;
+    @BindView(R.id.occupationTextView) TextView mOccupationLabel;
+    @BindView(R.id.appearanceTextView) TextView mAppearanceLabel;
+    @BindView(R.id.birthdayTextView) TextView mBirthdayLabel;
+    @BindView(R.id.nicknameTextView) TextView mNicknameLabel;
+    @BindView(R.id. statusTextView) TextView mStatusLabel;
+    @BindView(R.id.saveCharacterButton) TextView mSaveCharacterButton;
 
+    private Character mCharacter;
 
     public CharacterDetailFragment() {
         // Required empty public constructor
     }
 
+    public static CharacterDetailFragment newInstance(Character character) {
+            CharacterDetailFragment characterDetailFragment = new CharacterDetailFragment();
+        Bundle args = new Bundle();
+        args.putParcelable("restaurant", Parcels.wrap(character));
+        characterDetailFragment.setArguments(args);
+        return characterDetailFragment;
+    }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_character_detail, container, false);
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mCharacter = Parcels.unwrap(getArguments().getParcelable("character"));
+    }
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_character_detail, container, false);
+        ButterKnife.bind(this, view);
+
+        Picasso.get().load(mCharacter.getImg()).into(mImageLabel);
+
+        List<String> occupations = mCharacter.getOccupation();
+        List<Integer> appearances = mCharacter.getAppearance();
+
+
+        mNameLabel.setText(mCharacter.getName());
+        mNicknameLabel.setText(mCharacter.getNickname());
+        mBirthdayLabel.setText(mCharacter.getBirthday());
+        mStatusLabel.setText(mCharacter.getStatus());
+        mOccupationLabel.setText(android.text.TextUtils.join(", ", occupations));
+        mAppearanceLabel.setText(android.text.TextUtils.join(", ", appearances));
+
+
+        return view;
     }
 
 }
