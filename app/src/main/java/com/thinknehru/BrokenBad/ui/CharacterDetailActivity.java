6ac843394b0460ad48336a4d9b02c0f0d56993f7
thinknehru <thinknehru@gmail.com>
2020-02-10 23:18:34 +0300

    Bind views in the Character detail activity

--- app/src/main/java/com/thinknehru/BrokenBad/ui/CharacterDetailActivity.java
+++ app/src/main/java/com/thinknehru/BrokenBad/ui/CharacterDetailActivity.java
@@ -1,16 +1,30 @@
 package com.thinknehru.BrokenBad.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
 import com.thinknehru.BrokenBad.R;
+import com.thinknehru.BrokenBad.adapters.CharacterPagerAdapter;
+import com.thinknehru.BrokenBad.models.Character;
+
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class CharacterDetailActivity extends AppCompatActivity {
+
+    @BindView(R.id.viewPager)
+    ViewPager mViewPager;
+    private CharacterPagerAdapter adapterViewPager;
+    List<Character> mCharacters;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_character_detail);
+        ButterKnife.bind(this);
     }
 }
