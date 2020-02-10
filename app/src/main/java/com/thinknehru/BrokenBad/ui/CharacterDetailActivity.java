18c780a1e0dcc1caae14d65189812ea81046561f
thinknehru <thinknehru@gmail.com>
2020-02-10 23:24:01 +0300

    Set the pager adapter in the character detail activity

--- app/src/main/java/com/thinknehru/BrokenBad/ui/CharacterDetailActivity.java
+++ app/src/main/java/com/thinknehru/BrokenBad/ui/CharacterDetailActivity.java
@@ -1,6 +1,7 @@
 package com.thinknehru.BrokenBad.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentPagerAdapter;
 import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
@@ -8,6 +9,8 @@ import android.os.Bundle;
 import com.thinknehru.BrokenBad.R;
 import com.thinknehru.BrokenBad.adapters.CharacterPagerAdapter;
 import com.thinknehru.BrokenBad.models.Character;
+
+import org.parceler.Parcels;
 
 import java.util.List;
 
@@ -26,5 +29,14 @@ public class CharacterDetailActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_character_detail);
         ButterKnife.bind(this);
+
+        mCharacters = Parcels.unwrap(getIntent().getParcelableExtra("characters"));
+
+        int startingPosition = getIntent().getIntExtra("position", 0);
+
+        adapterViewPager = new CharacterPagerAdapter(getSupportFragmentManager(), FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT, mCharacters);
+        mViewPager.setAdapter(adapterViewPager);
+        mViewPager.setCurrentItem(startingPosition);
+
     }
 }
