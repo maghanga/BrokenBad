8fcb61c4a97759ac56c89adae25a7f7ad17774e3
thinknehru <thinknehru@gmail.com>
2020-02-10 17:29:59 +0300

    Create a character detail fragment

---
+++ app/src/main/java/com/thinknehru/BrokenBad/ui/CharacterDetailFragment.java
@@ -0,0 +1,33 @@
+package com.thinknehru.BrokenBad.ui;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.thinknehru.BrokenBad.R;
+import com.thinknehru.brokenbad.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class CharacterDetailFragment extends Fragment {
+
+
+    public CharacterDetailFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_character_detail, container, false);
+    }
+
+}
