e0d0d9a2a41ee721c52c5f05ff44158bd8caadc9
thinknehru <thinknehru@gmail.com>
2020-02-15 13:09:45 +0300

    Add member variables to hold the view and context which we set in the constructor of the FirebaseCharacterViewHolder

---
+++ app/src/main/java/com/thinknehru/BrokenBad/adapters/FirebaseCharacterViewHolder.java
@@ -0,0 +1,65 @@
+package com.thinknehru.BrokenBad.adapters;
+
+import android.content.Context;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.squareup.picasso.Picasso;
+import com.thinknehru.BrokenBad.R;
+import com.thinknehru.BrokenBad.models.Character;
+
+public class FirebaseCharacterViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+    View mView;
+    Context mContext;
+
+    public FirebaseCharacterViewHolder(View itemView) {
+        super(itemView);
+        mView = itemView;
+        mContext = itemView.getContext();
+        itemView.setOnClickListener(this);
+    }
+
+    public void bindCharacter(Character character) {
+        ImageView characterImageView = (ImageView) mView.findViewById(R.id.characterImageView);
+        TextView nameTextView = (TextView) mView.findViewById(R.id.characterNameTextView);
+        TextView nicknameTextView = (TextView) mView.findViewById(R.id.nicknameTextView);
+        TextView statusTextView = (TextView) mView.findViewById(R.id.statusTextView);
+
+        Picasso.get().load(character.getImg()).into(characterImageView);
+
+        nameTextView.setText(character.getName());
+        nicknameTextView.setText(character.getNickname());
+        statusTextView.setText("Rating: " + character.getStatus());
+    }
+
+    @Override
+    public void onClick(View view) {
+        final ArrayList<Restaurant> restaurants = new ArrayList<>();
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference(Constants.FIREBASE_CHILD_RESTAURANTS);
+        ref.addListenerForSingleValueEvent(new ValueEventListener() {
+
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                    restaurants.add(snapshot.getValue(Restaurant.class));
+                }
+
+                int itemPosition = getLayoutPosition();
+
+                Intent intent = new Intent(mContext, RestaurantDetailActivity.class);
+                intent.putExtra("position", itemPosition + "");
+                intent.putExtra("restaurants", Parcels.wrap(restaurants));
+
+                mContext.startActivity(intent);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+            }
+        });
+    }
+}
