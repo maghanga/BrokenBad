767ebb6ac400c41fe49f3b94fbcfd1badc203f93
thinknehru <thinknehru@gmail.com>
2020-02-15 13:49:32 +0300

    Initialize the DatabaseReference, FirebaseRecyclerAdapter, and RecyclerView member variables

--- app/src/main/java/com/thinknehru/BrokenBad/ui/FavouriteCharactersListActivity.java
+++ app/src/main/java/com/thinknehru/BrokenBad/ui/FavouriteCharactersListActivity.java
@@ -4,9 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.google.firebase.database.DatabaseReference;
 import com.thinknehru.BrokenBad.R;
+import com.thinknehru.BrokenBad.adapters.FirebaseCharacterViewHolder;
+import com.thinknehru.BrokenBad.models.Character;
 
 public class FavouriteCharactersListActivity extends AppCompatActivity {
+
+    private DatabaseReference mCharacterReference;
+    private FirebaseRecyclerAdapter<Character, FirebaseCharacterViewHolder> mFirebaseAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
