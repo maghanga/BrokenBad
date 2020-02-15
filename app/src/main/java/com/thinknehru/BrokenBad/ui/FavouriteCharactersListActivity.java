1c5d546f09178ecd091ed7542a493ee1474e4161
thinknehru <thinknehru@gmail.com>
2020-02-15 14:03:07 +0300

    Create a setUpFirebaseAdapter method and call it in the onCreate method

--- app/src/main/java/com/thinknehru/BrokenBad/ui/FavouriteCharactersListActivity.java
+++ app/src/main/java/com/thinknehru/BrokenBad/ui/FavouriteCharactersListActivity.java
@@ -1,11 +1,17 @@
 package com.thinknehru.BrokenBad.ui;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.thinknehru.BrokenBad.R;
@@ -32,5 +38,30 @@ public class FavouriteCharactersListActivity extends AppCompatActivity {
         ButterKnife.bind(this);
 
         mCharacterReference = FirebaseDatabase.getInstance().getReference(Constants.FIREBASE_CHILD_CHARACTERS);
+        setUpFirebaseAdapter();
+    }
+
+    private void setUpFirebaseAdapter(){
+        FirebaseRecyclerOptions<Character> options =
+                new FirebaseRecyclerOptions.Builder<Character>()
+                        .setQuery(mCharacterReference, Character.class)
+                        .build();
+
+        mFirebaseAdapter = new FirebaseRecyclerAdapter<Character, FirebaseCharacterViewHolder>(options) {
+            @Override
+            protected void onBindViewHolder(@NonNull FirebaseCharacterViewHolder firebaseCharacterViewHolder, int position, @NonNull Character character) {
+                firebaseCharacterViewHolder.bindCharacter(character);
+            }
+
+            @NonNull
+            @Override
+            public FirebaseCharacterViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.character_list_item, parent, false);
+                return new FirebaseCharacterViewHolder(view);
+            }
+        };
+
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        mRecyclerView.setAdapter(mFirebaseAdapter);
     }
 }
