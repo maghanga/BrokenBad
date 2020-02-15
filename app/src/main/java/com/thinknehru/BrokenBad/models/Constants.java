bb414ed8f2e688f9ebc51bd9794eecdfab67d4a3
thinknehru <thinknehru@gmail.com>
2020-02-15 12:20:27 +0300

    Define the child name of the Firebase node we'd like to save information to in Constants class

--- app/src/main/java/com/thinknehru/BrokenBad/models/Constants.java
+++ app/src/main/java/com/thinknehru/BrokenBad/models/Constants.java
@@ -2,4 +2,5 @@ package com.thinknehru.BrokenBad.models;
 
 public class Constants {
     public static final String YELP_BASE_URL = "https://www.breakingbadapi.com/api/";
+    public static final String FIREBASE_CHILD_SEARCHED_LOCATION = "searchedLocation";
 }
