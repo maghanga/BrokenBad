f075f248a1d18b323d3762e4318990fe9be50a49
thinknehru <thinknehru@gmail.com>
2020-02-15 19:41:04 +0300

    Implement the View.OnClickListener in the Create account activity

--- app/src/main/java/com/thinknehru/BrokenBad/ui/CreateAccountActivity.java
+++ app/src/main/java/com/thinknehru/BrokenBad/ui/CreateAccountActivity.java
@@ -3,10 +3,11 @@ package com.thinknehru.BrokenBad.ui;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 import com.thinknehru.BrokenBad.R;
 
-public class CreateAccountActivity extends AppCompatActivity {
+public class CreateAccountActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
