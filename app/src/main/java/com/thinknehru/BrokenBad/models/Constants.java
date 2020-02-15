619cdc070804761da8e09d358cc8b84d419edcf3
thinknehru <thinknehru@gmail.com>
2020-02-15 12:32:10 +0300

    Create a node name for the favourite characters in the Constants class

--- app/src/main/java/com/thinknehru/BrokenBad/models/Constants.java
+++ app/src/main/java/com/thinknehru/BrokenBad/models/Constants.java
@@ -2,5 +2,5 @@ package com.thinknehru.BrokenBad.models;
 
 public class Constants {
     public static final String YELP_BASE_URL = "https://www.breakingbadapi.com/api/";
-    public static final String FIREBASE_CHILD_SEARCHED_LOCATION = "searchedLocation";
+    public static final String FIREBASE_CHILD_CHARACTERS = "characters";
 }
--- app/src/main/java/com/thinknehru/BrokenBad/ui/MainActivity.java
+++ app/src/main/java/com/thinknehru/BrokenBad/ui/MainActivity.java
@@ -7,18 +7,30 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.thinknehru.BrokenBad.R;
+import com.thinknehru.BrokenBad.models.Constants;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+//    private DatabaseReference mSearchedCharacterReference;
+
     @BindView(R.id.viewCharactersButton)
     Button mViewCharactersButton;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+//        mSearchedCharacterReference = FirebaseDatabase
+//                .getInstance()
+//                .getReference()
+//                .child(Constants.FIREBASE_CHILD_SEARCHED_LOCATION);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
