643636c3776b0b1a04ebe833ac7198c7c7a56c3d
thinknehru <thinknehru@gmail.com>
2020-02-15 13:37:00 +0300

    Replace the mistaken favourite characters class with an activity

--- app/src/main/java/com/thinknehru/BrokenBad/ui/FavouriteCharactersListActivity.java
+++ app/src/main/java/com/thinknehru/BrokenBad/ui/FavouriteCharactersListActivity.java
@@ -1,4 +1,16 @@
 package com.thinknehru.BrokenBad.ui;
 
-public class FavouriteCharactersListActivity {
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.thinknehru.BrokenBad.R;
+
+public class FavouriteCharactersListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_favourite_characters_list);
+    }
 }
