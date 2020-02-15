8b706a2052dcea636ae6d86b5d97ab467514592f
thinknehru <thinknehru@gmail.com>
2020-02-15 13:50:50 +0300

    Bind the RecyclerView in the Favourite characters activity

--- app/src/main/java/com/thinknehru/BrokenBad/ui/FavouriteCharactersListActivity.java
+++ app/src/main/java/com/thinknehru/BrokenBad/ui/FavouriteCharactersListActivity.java
@@ -1,6 +1,7 @@
 package com.thinknehru.BrokenBad.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
@@ -10,14 +11,22 @@ import com.thinknehru.BrokenBad.R;
 import com.thinknehru.BrokenBad.adapters.FirebaseCharacterViewHolder;
 import com.thinknehru.BrokenBad.models.Character;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class FavouriteCharactersListActivity extends AppCompatActivity {
 
     private DatabaseReference mCharacterReference;
     private FirebaseRecyclerAdapter<Character, FirebaseCharacterViewHolder> mFirebaseAdapter;
 
+    @BindView(R.id.recyclerView)
+    RecyclerView mRecyclerView;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_character_list);
+        ButterKnife.bind(this);
     }
 }
