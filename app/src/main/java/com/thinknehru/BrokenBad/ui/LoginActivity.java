3407285f8362af514b6240d78d4eb0c1a5a19051
thinknehru <thinknehru@gmail.com>
2020-02-15 19:19:07 +0300

    Bind the create account txtview in the login activity and set an onclick listener on it

--- app/src/main/java/com/thinknehru/BrokenBad/ui/LoginActivity.java
+++ app/src/main/java/com/thinknehru/BrokenBad/ui/LoginActivity.java
@@ -4,14 +4,25 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import com.thinknehru.BrokenBad.R;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @BindView(R.id.registerTextView)
+    TextView mRegisterTextView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
+
+        mRegisterTextView.setOnClickListener(this);
     }
 }
