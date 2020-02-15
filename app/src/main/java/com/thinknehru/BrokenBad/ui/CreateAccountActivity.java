771d0e29b1c074f749c51d12bd04a71bf8e55d39
thinknehru <thinknehru@gmail.com>
2020-02-15 19:42:19 +0300

    Bind the views in the create account activity and set onclick listeners to the button and login textview

--- app/src/main/java/com/thinknehru/BrokenBad/ui/CreateAccountActivity.java
+++ app/src/main/java/com/thinknehru/BrokenBad/ui/CreateAccountActivity.java
@@ -4,14 +4,34 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.thinknehru.BrokenBad.R;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class CreateAccountActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @BindView(R.id.createUserButton)
+    Button mCreateUserButton;
+    @BindView(R.id.nameEditText)
+    EditText mNameEditText;
+    @BindView(R.id.emailEditText) EditText mEmailEditText;
+    @BindView(R.id.passwordEditText) EditText mPasswordEditText;
+    @BindView(R.id.confirmPasswordEditText) EditText mConfirmPasswordEditText;
+    @BindView(R.id.loginTextView)
+    TextView mLoginTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
+        ButterKnife.bind(this);
+
+        mLoginTextView.setOnClickListener(this);
+        mCreateUserButton.setOnClickListener(this);
     }
 }
