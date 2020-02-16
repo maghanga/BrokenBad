0fe55be53805b60ba2b8079945fde566007c359f
thinknehru <thinknehru@gmail.com>
2020-02-16 09:32:36 +0300

    Add a member variable to get the instance of the FirebaseAuth object

--- app/src/main/java/com/thinknehru/BrokenBad/ui/CreateAccountActivity.java
+++ app/src/main/java/com/thinknehru/BrokenBad/ui/CreateAccountActivity.java
@@ -1,20 +1,30 @@
 package com.thinknehru.BrokenBad.ui;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 import com.thinknehru.BrokenBad.R;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class CreateAccountActivity extends AppCompatActivity implements View.OnClickListener {
+
+    public static final String TAG = CreateAccountActivity.class.getSimpleName();
+    private FirebaseAuth mAuth;
 
     @BindView(R.id.createUserButton)
     Button mCreateUserButton;
@@ -52,5 +62,25 @@ public class CreateAccountActivity extends AppCompatActivity implements View.OnC
         }
 
     }
+
+//    private void createNewUser() {
+//        final String name = mNameEditText.getText().toString().trim();
+//        final String email = mEmailEditText.getText().toString().trim();
+//        String password = mPasswordEditText.getText().toString().trim();
+//        String confirmPassword = mConfirmPasswordEditText.getText().toString().trim();
+//
+//        mAuth.createUserWithEmailAndPassword(email, password)
+//                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+//                    @Override
+//                    public void onComplete(@NonNull Task<AuthResult> task) {
+//                        if (task.isSuccessful()) {
+//                            Log.d(TAG, "Authentication successful");
+//                        } else {
+//                            Toast.makeText(CreateAccountActivity.this, "Authentication failed.",
+//                                    Toast.LENGTH_SHORT).show();
+//                        }
+//                    }
+//                });
+//    }
 
 }
