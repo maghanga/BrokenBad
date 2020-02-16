b3e41d0439ddbe4fa456246c4eb57ae3b18342c1
thinknehru <thinknehru@gmail.com>
2020-02-16 09:40:34 +0300

    Add onStart and onStop lifecycle methods to ad or remove the AuthStateListener depending on the activity's lifecycle

--- app/src/main/java/com/thinknehru/BrokenBad/ui/CreateAccountActivity.java
+++ app/src/main/java/com/thinknehru/BrokenBad/ui/CreateAccountActivity.java
@@ -104,4 +104,17 @@ public class CreateAccountActivity extends AppCompatActivity implements View.OnC
     }
 
 
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
+    }
 }
