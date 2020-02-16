d1fd2b2ccdaa58af41391e522c97014ce07eac0f
thinknehru <thinknehru@gmail.com>
2020-02-16 09:54:27 +0300

    Inflate the overflow menu in an onCreateOptionsMenu() method within MainActivity

--- app/src/main/java/com/thinknehru/BrokenBad/ui/MainActivity.java
+++ app/src/main/java/com/thinknehru/BrokenBad/ui/MainActivity.java
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
@@ -40,6 +43,23 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         mFavouriteCharactersButton.setOnClickListener(this);
 
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        int id = item.getItemId();
+//        if (id == R.id.action_logout) {
+//            logout();
+//            return true;
+//        }
+//        return super.onOptionsItemSelected(item);
+//    }
 
     @Override
     public  void onClick(View v){
