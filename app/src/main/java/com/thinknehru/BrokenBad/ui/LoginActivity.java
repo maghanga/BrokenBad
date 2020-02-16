7ee3abf77faf8cde6afb94194546cda6a5101781
thinknehru <thinknehru@gmail.com>
2020-02-16 11:03:05 +0300

    Bind all the views in the login activity

--- app/src/main/java/com/thinknehru/BrokenBad/ui/LoginActivity.java
+++ app/src/main/java/com/thinknehru/BrokenBad/ui/LoginActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.thinknehru.BrokenBad.R;
@@ -14,8 +16,12 @@ import butterknife.ButterKnife;
 
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
 
-    @BindView(R.id.registerTextView)
-    TextView mRegisterTextView;
+    @BindView(R.id.passwordLoginButton)
+    Button mPasswordLoginButton;
+    @BindView(R.id.emailEditText) EditText mEmailEditText;
+    @BindView(R.id.passwordEditText)
+    EditText mPasswordEditText;
+    @BindView(R.id.registerTextView) TextView mRegisterTextView;
 
 
     @Override
