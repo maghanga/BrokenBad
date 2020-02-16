347a020f3680cba7cb3787aaff0bd5f6a5811c81
thinknehru <thinknehru@gmail.com>
2020-02-16 11:07:21 +0300

    Add a loginWithPassword() method and call it when the mPasswordLoginButton has been clicked

--- app/src/main/java/com/thinknehru/BrokenBad/ui/LoginActivity.java
+++ app/src/main/java/com/thinknehru/BrokenBad/ui/LoginActivity.java
@@ -45,5 +45,22 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
             startActivity(intent);
             finish();
         }
+
+        if (view == mPasswordLoginButton) {
+            loginWithPassword();
+        }
+    }
+
+    private void loginWithPassword() {
+        String email = mEmailEditText.getText().toString().trim();
+        String password = mPasswordEditText.getText().toString().trim();
+        if (email.equals("")) {
+            mEmailEditText.setError("Please enter your email");
+            return;
+        }
+        if (password.equals("")) {
+            mPasswordEditText.setError("Password cannot be blank");
+            return;
+        }
     }
 }
