0176313e65e14e3308c3431ace93a194adcd3206
thinknehru <thinknehru@gmail.com>
2020-02-16 09:38:29 +0300

    Create a createAuthStateListener method and call it in the onCreate method

--- app/src/main/java/com/thinknehru/BrokenBad/ui/CreateAccountActivity.java
+++ app/src/main/java/com/thinknehru/BrokenBad/ui/CreateAccountActivity.java
@@ -16,6 +16,7 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.thinknehru.BrokenBad.R;
 
 import butterknife.BindView;
@@ -45,6 +46,7 @@ public class CreateAccountActivity extends AppCompatActivity implements View.OnC
 
         mLoginTextView.setOnClickListener(this);
         mCreateUserButton.setOnClickListener(this);
+        createAuthStateListener();
     }
 
 
@@ -83,5 +85,23 @@ public class CreateAccountActivity extends AppCompatActivity implements View.OnC
                     }
                 });
     }
+
+    private void createAuthStateListener() {
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                final FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    Intent intent = new Intent(CreateAccountActivity.this, MainActivity.class);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                    startActivity(intent);
+                    finish();
+                }
+            }
+
+        };
+    }
+
 
 }
